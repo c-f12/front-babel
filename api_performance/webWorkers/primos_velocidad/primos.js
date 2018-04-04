30f8a242aa42c81efcdfa2ba6ebe627982396a55
clara fernandez <clara.fernandez@babel.es>
2018-04-04 13:53:03 +0200

    api webworkers

---
+++ api_performance/webWorkers/primos_velocidad/primos.js
@@ -0,0 +1,18 @@
+(function () {
+	// Buscar número primos
+	postMessage('primosSTART');
+	var n = 1;
+	postMessage(n);
+	seguirBuscando: while (n < 100000) {
+		n += 1;
+		var max = Math.sqrt(n)
+		for (var i = 2; i <= max; i += 1) {
+			if (n % i == 0) {
+				continue seguirBuscando;
+			}
+		}
+		//el worker tarda más en enviar un mensaje que en procesarla
+		postMessage(n);
+	}
+	postMessage('primosEND');
+}())
---
+++ api_performance/webWorkers/primos_velocidad/primosSpeed.js
@@ -0,0 +1,22 @@
+/* Optimizamos la aplicación. Enviamos un array al final
+en lugar de un mensaje  */
+(function () {
+	// Buscar número primos
+	postMessage('primosSTART');
+	var n = 1;
+	postMessage(n);
+	var primos = [];
+	primos.push(n);
+	seguirBuscando: while (n < 100000) {
+		n += 1;
+		var max = Math.sqrt(n)
+		for (var i = 2; i <= max; i += 1) {
+			if (n % i == 0) {
+				continue seguirBuscando;
+			}
+		}
+		primos.push(n);
+	}
+	postMessage(primos);
+	postMessage('primosEND');
+}())
