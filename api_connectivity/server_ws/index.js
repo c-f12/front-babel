2793ef2a8931611a18073bf1c4d8608807f3c1dc
clara fernandez <clara.fernandez@babel.es>
2018-04-05 12:34:25 +0200

    api connectivity

---
+++ api_connectivity/server_ws/index.js
@@ -0,0 +1,12 @@
+var WebSocketServer = require( 'ws' ).Server;
+var wss = new WebSocketServer({port: 8085});
+wss.on('connection', function(ws) {
+    ws.on('message', (message) => {
+        console.log(' received: %s ', message) ;
+        setTimeout(() => {
+            console.log(' resending: %s ', message) 
+            ws.send(`Hola ${message}`)
+        }, 500);
+    });
+    ws.send('something') ;
+});
